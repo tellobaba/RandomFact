cde6fcd7f02a97b627f53bfdc49ce2ee4594b652
Oluwatomiwa <olatunjitello17@gmail.com>
2023-02-09 19:52:00 +0100

    Initial Commit

---
+++ RandomFact/RandomFactApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomFactApp.swift
+//  RandomFact
+//
+//  Created by Oluwatomiwa on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomFactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
